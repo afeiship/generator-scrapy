9228bdc25d99bcfd5af9449c056a4ddbec1b7d20
afeiship <1290657123@qq.com>
2022-04-08 17:35:35 +0800

    wip

--- generators/app/templates/ecosystem.config.js
+++ generators/app/templates/ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
   apps: [
     {
       name: 'entry',
-      // interpreter: 'bash',
       namespace: 'aizhan',
       script: 'poetry run scrapy crawl entry',
       ignore_watch: ['node_modules', 'logs', 'tmp', '*.pyc']
