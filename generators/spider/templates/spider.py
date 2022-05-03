e4b1d0f963e6975f97662a6aab6d92b42cef625f
afeiship <1290657123@qq.com>
2022-05-03 15:32:51 +0800

    feat: upt spider pm2 start/stop

--- generators/spider/templates/spider.py
+++ generators/spider/templates/spider.py
@@ -1,10 +1,28 @@
 import scrapy
+import jsw_nx as nx
+from jsw_scrapy.spiders.base_spider import BaseSpider
 
 
-class <%- ctx.classify(model_name) %>Spider(scrapy.Spider):
+class <%- ctx.classify(model_name) %>Spider(BaseSpider):
     name = '<%= model_name %>'
     handle_httpstatus_list = [400]
     start_urls = ['https://www.baidu.com/']
 
-    def parse(self, response):
-        pass
+    custom_settings = {
+        'CONCURRENT_REQUESTS': 100,
+    }
+
+    @property
+    def records(self):
+        return self.get_un_crawled(entity_class='XyClass')
+
+    @property
+    def is_done(self):
+        return len(self.records) == 0
+
+    def start_requests(self):
+      records = self.records
+      if self.is_done:
+          nx.Pm2Manager.stop(self.name)
+          self.logger.info('All records are crawled')
+      pass
