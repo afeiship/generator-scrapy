351e2fce23d829e4cd802f539148a0154ae89d32
afeiship <1290657123@qq.com>
2022-10-23 16:56:44 +0800

    feat: add pweespider

--- generators/spider/index.js
+++ generators/spider/index.js
@@ -7,15 +7,16 @@ module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
     this.argument("model_name", { type: String, required: true });
+    this.option("orm", { type: String, required: true });
   }
 
   writing() {
-    const { model_name } = this.options;
+    const { model_name, orm } = this.options;
     const dirs = globby.sync("**/spiders", { onlyDirectories: true });
     const target = dirs.find(item => item.endsWith("spiders"));
 
     this.fs.copyTpl(
-      this.templatePath("spider.py"),
+      this.templatePath(`${orm}/spider.py`),
       this.destinationPath(`${target}/${model_name}.py`),
       { model_name, ctx: yoHelper.ctx }
     );
