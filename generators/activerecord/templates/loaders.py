84fa385e784d9f59f9b1700609808a09e4b58f32
afeiship <1290657123@qq.com>
2024-02-29 15:02:57 +0800

    feat: upt active reodcrd

--- generators/activerecord/templates/loaders.py
+++
@@ -1,5 +0,0 @@
-from scrapy.loader import ItemLoader
-from itemloaders.processors import MapCompose, TakeFirst, Join, Identity
-
-class <%- ctx.classify(app_name) %>Loader(ItemLoader):
-  default_output_processor = Identity()
